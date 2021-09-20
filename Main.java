45b183e2f6531e7714ae58356fbfb5dae79eab5e
Gust2491 <69625970+Gust2491@users.noreply.github.com>
2021-09-20 14:20:38 +0200

    opg 4 not done

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
 
 class Main {
-  public static void main(String[] args) {
 
+  public static void main(String[] args) {
+    Linkedlist coolList = new LinkedList();
+    Node n1 = new Node();
+    n1.value = 1;
+    Node n2 = new Node();
+    n2.value = 2;
+    Node n3 = new Node();
+    n3.value = 3;
+    coolList.first = n1;
+    n1.next = n2;
+    n2.next = n3;
+    n3.next = null;
+    coolList.print();
   }
 
-  Node n1 = new Node();
-  Node n2 = new Node();
-  Node n3 = new Node();
-  n1.value = 1;
-  n1.next = n2;
-  n2.value = 2;
-  n2.next = n3;
-  n3.value = 3;
-  n3.next = null;
+
 
 }
