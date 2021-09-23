6c0837863b95c4af7b022d21a99b1124cfa0d3e5
Oliver Christensen <orc13a@live.dk>
2021-09-23 08:35:49 +0200

    fix

--- LinkedList.java
+++ LinkedList.java
@@ -1,18 +1,15 @@
 class LinkedList {
-  Node first;
+    Node first;
 
-  public static void main(String[] args) {
-  }
-
-  public static void print(){
-    System.out.println("hey");
-    //Node link = first.next;
-  //  System.out.print(first.value + ", ");
-    while(link.next != null){
-    //  System.out.print(link.value + ", ");
-      link = link.next;
-
+    public static void main(String[] args) {
     }
 
-  }
+    public static void print(){
+        System.out.println("hey");
+        // Node link = first.next;
+        // System.out.print(first.value + ", ");
+        // while(link.next != null){
+        // System.out.print(link.value + ", ");
+        //   link = link.next;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,21 +1,19 @@
-
 class Main {
+    // LinkedList coolList;
 
-  public static void main(String[] args) {
-    Linkedlist coolList = new LinkedList();
-    Node n1 = new Node();
-    n1.value = 1;
-    Node n2 = new Node();
-    n2.value = 2;
-    Node n3 = new Node();
-    n3.value = 3;
-    coolList.first = n1;
-    n1.next = n2;
-    n2.next = n3;
-    n3.next = null;
-    coolList.print();
-  }
+    public static void main(String[] args) {
+        LinkedList coolList = new LinkedList();
+        Node n1 = new Node();
+        n1.value = 1;
+        Node n2 = new Node();
+        n2.value = 2;
+        Node n3 = new Node();
+        n3.value = 3;
+        coolList.first = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = null;
 
-
-
+        coolList.print();
+    }
 }
