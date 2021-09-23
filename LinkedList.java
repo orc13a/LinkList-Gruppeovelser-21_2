dfed871f1f2cc1c20d9676adcca15fa02c09c3e9
Oliver Christensen <orc13a@live.dk>
2021-09-23 18:29:47 +0200

    fixed

--- LinkedList.java
+++ LinkedList.java
@@ -1,52 +1,43 @@
 class LinkedList {
     Node first;
 
-    //Metode der printer værdierne af vores nodes i vores liste
     public void print() {
         boolean listEnd = false;
-        System.out.println("");
+        Node node = first;
+        String result = "[";
 
-        //link variablen holder den næste node i listen
-        Node link = first.next;
-
-        //Her printes første node værdi
-        System.out.print("[" + first.value + ", ");
-
-        //Et while loop, som tjekker om vi er nået ved enden af listen
         while (listEnd == false) {
-            //if statement som tjekker om vi er ved den sidste node i listen
-            if (link.next == null) {
-                System.out.print(link.value + "]");
+            if (node.next == null) {
+                result += (node.value + "]");
                 listEnd = true;
             } else {
-                System.out.print(link.value + ", ");
+                result += (node.value + ", ");
             }
 
-            //Det der gør at vi går videre i listen
-            link = link.next;
+            node = node.next;
         }
 
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\n" + result + "\n");
     }
 
     public void add(int v){
       Node n = new Node();
       n.value = v;
 
-      if(first == null){
-        first = n;
+      if (first == null) {
+          first = n;
       } else {
-        boolean listEnd = false;
-        Node link = first.next;
+          boolean listEnd = false;
+          Node node = first;
 
-        while(listEnd == false){
-          if(link.next == null){
-            link.next = n;
-          } else {
-            link = link.next;
+          while (listEnd == false) {
+              if (node.next == null) {
+                  node.next = n;
+                  listEnd = true;
+              } else {
+                  node = node.next;
+              }
           }
-        }
       }
     }
 }
--- Main.java
+++ Main.java
@@ -1,20 +1,13 @@
 class Main {
-    // LinkedList coolList;
-
     public static void main(String[] args) {
         LinkedList coolList = new LinkedList();
-      //  Node n1 = new Node();
-      //  n1.value = 1;
-      //  Node n2 = new Node();
-      //  n2.value = 2;
-      //  Node n3 = new Node();
-      //  n3.value = 3;
-      //  coolList.first = n1;
-      //  n1.next = n2;
-      //  n2.next = n3;
-      //  n3.next = null;
+
         coolList.add(1);
-        
+        coolList.add(2);
+        coolList.add(3);
+        coolList.add(4);
+        coolList.add(5);
+
         coolList.print();
     }
 }
