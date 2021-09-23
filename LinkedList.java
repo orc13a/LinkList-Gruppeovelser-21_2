4f69ac9c104a17af555f4fc8fe4e52c334135872
Oliver Christensen <orc13a@live.dk>
2021-09-23 08:51:33 +0200

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,17 @@
 class LinkedList {
     Node first;
 
-    public static void main(String[] args) {
-    }
+    public void print() {
+        System.out.println("");
 
-    public static void print(){
-      //  System.out.println("hey");
         Node link = first.next;
         System.out.print(first.value + ", ");
-        while(link.next != null){
-        System.out.print(link.value + ", ");
-        link = link.next;
+
+        while(link.next != null) {
+            System.out.print(link.value + ", ");
+            link = link.next;
+        }
+
+        System.out.println("");
     }
 }
